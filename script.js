dccaa144e76464c05c3f4e0856a5ff13fe03e5a9
Rouble <roublesamanta@gmail.com>
2022-05-12 20:55:52 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log("Halu, this works");
+
+/*function addThis(a, b) {
+
+    return a + b;
+    document.getElementById("demo").innerHTML = 15;
+}*/
+function addThis() {
+
+    document.getElementById("demo").innerHTML = "hi";
+}
