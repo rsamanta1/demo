f19a513baeb38aebfa5ace8314bfc1157fd6b108
rsamanta1 <49842283+rsamanta1@users.noreply.github.com>
2022-05-13 00:07:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 //console.log("Halu, this works");
 
-/*function addThis(a, b) {
+var opacity = 0;
+var myInterval = 0;
 
-    return a + b;
-    document.getElementById("demo").innerHTML = 15;
-}*/
+function fadeIn() {
+    myInterval = setInterval(show, 20);
+}
+function fadeOut() {
+    myInterval = setInterval(hide, 20);
+}
+
+function show() {
+    var img = document.getElementById("img1");
+    opacity = Number(window.getComputedStyle(img).getPropertyValue("opacity")); //fetch opacity
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        img.style.opacity = opacity;
+    }
+    else {
+        clearInterval(myInterval);
+    }
+}
+function hide() {
+    var img = document.getElementById("img1");
+    opacity = Number(window.getComputedStyle(img).getPropertyValue("opacity")); //fetch opacity
+    if (opacity > 0) {
+        opacity = opacity - 0.1;
+        img.style.opacity = opacity;
+    }
+    else {
+        clearInterval(myInterval);
+    }
+}
+
 function addThis() {
 
-    document.getElementById("demo").innerHTML = "hi";
+    document.getElementById("demo").innerHTML = "Great Job! Now let's see what else we can do...stay tuned.";
 }
